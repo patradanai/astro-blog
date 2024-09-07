924cdfa24631cd8cf09a713ce3ea54b5fb547150
Patradanai Nakpimay <patradanai.na@tnds.com>
2024-09-07 17:44:14 +0700

    init: project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import vercel from "@astrojs/vercel/serverless";
+
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://stargazers.club",
+  output: "server",
+  adapter: vercel(),
+  integrations: [
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
+    sitemap(),
+    mdx(),
+  ],
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,51 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+function withOpacityValue(variable) {
+  return ({ opacityVariable, opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variable}), ${opacityValue})`;
+    }
+    if (opacityVariable !== undefined) {
+      return `rgba(var(${variable}), var(${opacityVariable}, 1))`;
+    }
+    return `rgb(var(${variable}))`;
+  };
+}
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        kanit: ["Kanit", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        blue: withOpacityValue("--tw-color-blue"),
+        indigo: "#6610f2",
+        purple: "#6f42c1",
+        pink: "#e83e8c",
+        red: "#dc3545",
+        orange: "#fd7e14",
+        yellow: "#ffc107",
+        green: "#28a745",
+        teal: "#20c997",
+        cyan: "#17a2b8",
+        white: "#fff",
+        gray: "#6c757d",
+        "gray-dark": "#343a40",
+        primary: "#0747A6",
+        secondary: "#e1e1ef",
+        success: "#1dc9b7",
+        info: "#5578eb",
+        warning: "#ffb822",
+        danger: "#fd397a",
+        light: "#f8f9fa",
+        dark: "#343a40",
+      },
+    },
+  },
+  plugins: [],
+};
