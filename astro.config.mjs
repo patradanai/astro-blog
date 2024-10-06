e3b017d0894647284cabec1e9ffc2831b6f9c7ab
Patradanai Nakpimay <patradanai.na@tnds.com>
2024-10-07 02:58:20 +0700

    fix: site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 import icon from 'astro-icon';
 
