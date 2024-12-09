d0e88e28b8738f28aa735817ac8fd191938b64ee
Patradanai Nakpimay <patradanai.na@tnds.com>
2024-12-09 19:15:40 +0700

    feat: add toolbar

--- src/utils/markdownRender/index.ts
+++ src/utils/markdownRender/index.ts
@@ -19,14 +19,14 @@ export const serializeMarkdown = async (
 		heading(args): string {
 			// Only H2
 			if (args.depth === 2) {
-                const anchor = args.text.toLowerCase().replace(/[^\p{L}\p{M}\d]+/gu, '-');
+				const anchor = args.text.toLowerCase().replace(/[^\p{L}\p{M}\d]+/gu, '-');
 
-                toc.push({
-                    anchor: anchor,
-                    level: args.depth,
-                    text: args.text,
-                });
-            }
+				toc.push({
+					anchor: anchor,
+					level: args.depth,
+					text: args.text,
+				});
+			}
 			const text = this.parser.parseInline(args.tokens);
 
 			return `
@@ -34,14 +34,36 @@ export const serializeMarkdown = async (
                   ${text}
                 </h${args.depth}>`;
 		},
-    image({href, text, title}): string {
-      const imageTitle = title ? `<figcaption class="text-center">${title}</figcaption>` : '';
+		image({ href, text, title }): string {
+			const imageTitle = title ? `<figcaption class="text-center">${title}</figcaption>` : '';
 			return `
 				<figure>
 					<img src="${href}" alt="${text}" />
 					${imageTitle}
 				</figure>`;
-      },
+		},
+		code({ text, escaped, lang }): string {
+			const hljsClass = lang ? `language-${lang}` : '';
+			const toolbar = `
+                <div class="code-toolbar">
+                    <div class="toolbar">
+                        <div class="toolbar-item"></div>
+                        <div class="toolbar-item"></div>
+                        <div class="toolbar-item"></div>
+                    </div>
+                </div>
+            `;
+
+			return `
+                <figure class="code-block">
+                    ${toolbar}
+					<button class="copy-button">
+                        <span class="icon">📋</span> Copy
+                    </button>
+                    <div><pre><code class="hljs ${hljsClass}">${escaped ? text : escape(text)}</code></pre></div>
+                </figure>
+            `;
+		},
 	};
 
 	marked.use(
@@ -49,7 +71,7 @@ export const serializeMarkdown = async (
 		markedHighlight({
 			async: true,
 			langPrefix: 'hljs language-',
-			highlight(code, lang, info) {
+			highlight(code, lang, _info) {
 				const language = hljs.getLanguage(lang) ? lang : 'plaintext';
 				return hljs.highlight(code, { language }).value;
 			},
@@ -61,3 +83,14 @@ export const serializeMarkdown = async (
 
 	return { contentRender, toc };
 };
+
+
+// Helper function to escape HTML
+function escape(html: string): string {
+	return html
+		.replace(/&/g, '&amp;')
+		.replace(/</g, '&lt;')
+		.replace(/>/g, '&gt;')
+		.replace(/"/g, '&quot;')
+		.replace(/'/g, '&#039;');
+}
