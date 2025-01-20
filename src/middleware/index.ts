0bfd06ab5ea097131bfb08bd949afe2edaf3de72
Patradanai Nakpimay <patradanai@Patradanais-MacBook-Pro.local>
2025-01-21 04:33:32 +0700

    fix

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -3,4 +3,4 @@ import { loggerMiddleware } from './logger';
 import { CacheMiddleware } from './cache';
 
 // `context` and `next` are automatically typed
-export const onRequest = sequence(loggerMiddleware, CacheMiddleware);
+export const onRequest = sequence(loggerMiddleware);
